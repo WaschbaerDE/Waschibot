5ab0e80a942e1e24cb1d914117fa141161a24f1e
WaschbaerDe <martinheyn1@web.de>
2021-01-22 19:54:41 +0100

    First Commit

--- src/main/java/me/name/bot/Token.java
+++ src/main/java/me/name/bot/Token.java
@@ -2,6 +2,6 @@ package me.name.bot;
 
 public class Token {
     public String getOAuth2Token(){
-        return "NTAwMzU2MjYzMTU5NTI5NDgy.W8DXFQ.QzSG5O8gpO7vrh6HKV4IWOnby1Q";
+        return "NTAwMzU2MjYzMTU5NTI5NDgy.W8DXFQ.xdbCzPuQjrIqik6rKOZD1OLsKsw";
     }
 }
