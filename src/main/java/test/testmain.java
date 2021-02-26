8ffa295fc04d3ecb0b257e89b4ed6c58d9d6075b
WaschbaerDE <44826829+WaschbaerDE@users.noreply.github.com>
2021-02-27 00:19:05 +0100

    Added numbers to randompicevent

--- src/main/java/test/testmain.java
+++ src/main/java/test/testmain.java
@@ -2,8 +2,22 @@ package test;
 
 public class testmain {
     public static void main(String[] args){
-
-
-        System.out.println(Math.random());
+        for(int i=0;i<100;i++){
+            System.out.println(generateRndChar());
+        }
     }
+
+        public static char generateRndLetter(){return (char)((int)(Math.random()*26)+97);}
+        public static char generateRndDigit(){return(char)((int)(Math.random()*10)+48); }
+
+
+        //get random [a-z]or[0-9]
+        public static char generateRndChar(){
+            if(Math.random()*37<27){
+                return generateRndLetter();
+            }else{
+                return generateRndDigit();
+            }
+        }
+
 }
